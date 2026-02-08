da658063937c641fba2ca1e9f756bdff2af366d9
Pranav Ghodake <pranavghodake3@gmail.com>
2026-02-08 19:44:11 +0530

    Added Basic CRUD

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,10 +1,51 @@
+const { successResponse, errorResponse }= require('../utils/responseHelper');
+const tasks = [];
 
-const testFirstFunction = async (req, res) => {
-    return res.status(200).json([
-        1, 2, 3
-    ]);
+const taskController = {};
+
+taskController.getTasks = async (req, res) => {
+    try {
+        return successResponse(res, tasks);   
+    } catch (error) {
+        return errorResponse(res, error, error.message, error.statusCode);
+    }
 };
 
-module.exports = {
-    testFirstFunction,
+taskController.getTaskById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        return successResponse(res, tasks[id]);   
+    } catch (error) {
+        return errorResponse(res, error, error.message, error.statusCode);
+    }
 };
+
+taskController.createTask = async (req, res) => {
+    try {
+        const task = req.body;
+        tasks.push(task);
+        return successResponse(res, task);   
+    } catch (error) {
+        return errorResponse(res, error, error.message, error.statusCode);
+    }
+};
+
+taskController.updateTask = async (req, res) => {
+    try {
+        const task = req.body;
+        return successResponse(res, task);   
+    } catch (error) {
+        return errorResponse(res, error, error.message, error.statusCode);
+    }
+};
+
+taskController.deleteTask = async (req, res) => {
+    try {
+        const id = req.params.id;
+        return successResponse(res, tasks[id]);   
+    } catch (error) {
+        return errorResponse(res, error, error.message, error.statusCode);
+    }
+};
+
+module.exports = taskController;
--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/taskController")
 
-router.get("/", taskController.testFirstFunction);
+router.get("/", taskController.getTasks);
+
+router.get("/:id", taskController.getTaskById);
+
+router.post("/", taskController.createTask);
+
+router.put("/", taskController.updateTask);
+
+router.delete("/:id", taskController.deleteTask);
 
 module.exports = router;
