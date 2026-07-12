2a6c5c3a3735943ba8b916ea8ebbcbb7bd524065
Pranav Ghodake <pranavghodake3@gmail.com>
2026-07-12 14:44:04 +0530

    Added Styled Global Project drop down

--- frontend/src/compoenets/NavBar.jsx
+++ frontend/src/compoenets/NavBar.jsx
@@ -1,6 +1,6 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { destroyToken } from "../util/auth";
-import { ACTION_TYPES, ENTITIES } from "../constants";
+import { ACTION_TYPES, ENTITIES, GLOBAL_ROLES } from "../constants";
 import usePermission from "../hooks/usePermission";
 import { useAuthStore } from "../store/authStore";
 import { useState } from "react";
@@ -33,13 +33,15 @@ export default function NavBar() {
         <div className="logo"><NavLink to="/">Home</NavLink> { `${user.firstName} ${user.lastName} (${user?.globalRole?.name ?? ''})` } </div>
 
         <nav className="menu">
+          {![GLOBAL_ROLES.SUPER_ADMIN, GLOBAL_ROLES.COMPANY_ADMIN].includes(user.globalRole.name) &&
           <div>
-            <select name="projects" id="projects" value={globalProjectId} onChange={handleGlobalProjectChange}>
+            <select className="project-select" name="projects" id="projects" value={globalProjectId} onChange={handleGlobalProjectChange}>
               {user?.projectMembership?.map(pm => 
                 <option key={pm.projectId} value={pm.project.id}>{pm.project.name}</option>
               )}
             </select>
           </div>
+          }
           <NavLink to="/dashboard">Dashboard({ user.company?.name })</NavLink>
 
           <NavLink to="/projects">Projects</NavLink>
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 import Cookies from "js-cookie";
+import { GLOBAL_ROLES } from "../constants";
 
 const authApi = axios.create({
   baseURL: "http://localhost:5000/api",
@@ -12,8 +13,10 @@ export function setLoginStore(data) {
 
   setUserData(data.user);
   setAccessToken(data.accessToken);
-  const globalProjectId = Cookies.get('globalProjectId');
-  Cookies.set('globalProjectId', globalProjectId ? globalProjectId : (data.user?.projectMembership[0]?.projectId ?? 0));
+  if (![GLOBAL_ROLES.SUPER_ADMIN, GLOBAL_ROLES.COMPANY_ADMIN].includes(data.user.globalRole.name)) {
+   const globalProjectId = Cookies.get('globalProjectId');
+    Cookies.set('globalProjectId', globalProjectId ? globalProjectId : (data.user?.projectMembership[0]?.projectId ?? 0));
+  }
 
   const expireMinutes = parseInt(data.accessTokenExpiresIn, 10);
   const expiryMs = Date.now() + expireMinutes * 60 * 1000;
@@ -33,4 +36,5 @@ export function logout() {
   setAccessToken("");
   setAccessTokenExpiry("");
   setIsLoggedIn(false);
+  Cookies.remove('globalProjectId');
 }
