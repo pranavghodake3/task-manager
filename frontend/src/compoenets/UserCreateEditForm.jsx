fb20e8d421a2c9442a7f588fb4f01034728aacd2
Pranav Ghodake <pranavghodake3@gmail.com>
2026-07-20 19:08:08 +0530

    Add Validations,Success/Error to User Form

--- frontend/src/compoenets/UserCreateEditForm.jsx
+++ frontend/src/compoenets/UserCreateEditForm.jsx
@@ -1,28 +1,33 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 import { useAuthStore } from "../store/authStore";
+import { addUserSchema } from "../formSchemas/addUser";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function UserCreateEditForm({ mode, user }) {
     const accessToken = useAuthStore((state) => state.accessToken);
-    const [firstName, selectFirstName] = useState(() => user?.firstName ?? '');
-    const [lastName, selectLastName] = useState(() => user?.lastName ?? '');
-    const [email, selectEmail] = useState(() => user?.email ?? '');
-    const [password, selectPassword] = useState('');
-    const [confirmPassword, selectConfirmPassword] = useState('');
-    // const [formErrorMessage, setFormErrorMessage] = useState('');
     const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+      } = useForm({
+        resolver: zodResolver(addUserSchema),
+        mode: 'onChange',
+        defaultValues: {
+          firstName: user?.firstName ?? '',
+          lastName: user?.lastName ?? '',
+          email: user?.email ?? '',
+          password: '',
+          confirm_password: '',
+        },
+      });
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+    async function onSubmit(data) {
         try {
             if (mode === 'create') {
-                const response = await api.post(`/users`, {
-                    firstName,
-                    lastName,
-                    email,
-                    password,
-                }, {
+                const response = await api.post(`/users`, data, {
                     headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
@@ -31,12 +36,7 @@ export default function UserCreateEditForm({ mode, user }) {
                     navigate('/users');
                 }
             }else{
-                const response = await api.put(`/users/${user.id}`, {
-                    firstName,
-                    lastName,
-                    email,
-                    password,
-                }, {
+                const response = await api.put(`/users/${user.id}`, data, {
                     headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
@@ -52,35 +52,40 @@ export default function UserCreateEditForm({ mode, user }) {
         }
     }
     return (
-        <form className="auth-form" onSubmit={handleSubmit}>
+        <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
 
             <div className="form-group">
                 <label>First Name</label>
-                <input type="text" placeholder="Enter your first name" value={firstName} onChange={(e) => selectFirstName(e.target.value)} />
+                <input type="text" placeholder="Enter your first name" {...register('firstName')} />
+                {errors.firstName && <p className="error-text">{errors.firstName.message}</p>}
             </div>
 
             <div className="form-group">
                 <label>Last Name</label>
-                <input type="text" placeholder="Enter your last name" value={lastName} onChange={(e) => selectLastName(e.target.value)} />
+                <input type="text" placeholder="Enter your last name" {...register('lastName')} />
+                {errors.lastName && <p className="error-text">{errors.lastName.message}</p>}
             </div>
 
             <div className="form-group">
                 <label>Email</label>
-                <input type="email" placeholder="Enter your email" value={email} onChange={(e) => selectEmail(e.target.value)} />
+                <input type="text" placeholder="Enter your email" {...register('email')} />
+                {errors.email && <p className="error-text">{errors.email.message}</p>}
             </div>
 
             <div className="form-group">
                 <label>Password</label>
-                <input type="password" placeholder="Create password" value={password} onChange={(e) => selectPassword(e.target.value)} />
+                <input type="password" placeholder="Create password" {...register('password')} />
+                {errors.password && <p className="error-text">{errors.password.message}</p>}
             </div>
 
             <div className="form-group">
                 <label>Confirm Password</label>
-                <input type="password" placeholder="Confirm password" value={confirmPassword} onChange={(e) => selectConfirmPassword(e.target.value)} />
+                <input type="password" placeholder="Confirm password" {...register('confirm_password')} />
+                {errors.confirm_password && <p className="error-text">{errors.confirm_password.message}</p>}
             </div>
 
-            <button type="submit" className="auth-btn">
-                {mode === 'create' ? 'Submit' : 'Update'}
+            <button type="submit" className="auth-btn" disabled={isSubmitting}>
+                {mode === 'create' ? (isSubmitting ? 'Submitting...' : 'Submit') : (isSubmitting ? 'Updating...' : 'Update')}
             </button>
             <button type="button" className="auth-btn" onClick={() => navigate('/users')}>
                 Cancel
