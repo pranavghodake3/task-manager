09ad9215e60b56efd9bdca55dc213e8c49978b21
Pranav Ghodake <pranavghodake3@gmail.com>
2026-07-20 21:45:32 +0530

    Add Validations,Success/Error to Project Form

--- frontend/src/compoenets/ProjectCreateEditForm.jsx
+++ frontend/src/compoenets/ProjectCreateEditForm.jsx
@@ -2,29 +2,31 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 import { useAuthStore } from "../store/authStore";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createOrEditProjectSchema } from "../formSchemas/createOrEditProject";
 
 export default function ProjectCreateEditForm({ mode, project }) {
     const accessToken = useAuthStore((state) => state.accessToken);
-    const [name, setProjectName] = useState(() => project?.name ?? '');
-    const [description, setProjectDescription] = useState(() => project?.description ?? '');
-
     const [formErrorMessage, setFormErrorMessage] = useState('');
     const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm({
+        resolver: zodResolver(createOrEditProjectSchema),
+        mode: 'onChange',
+        defaultValues: {
+            name: project?.name ?? '',
+            description: project?.description ?? '',
+        },
+    });
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+    async function onSubmit(data) {
         try {
             if (mode === 'create') {
-                const response = await api.post('/projects', {
-                    name,
-                    description,
-                    // createNew,
-                    // projectAdminId: userId,
-                    // firstName,
-                    // lastName,
-                    // email,
-                    // password
-                }, {
+                const response = await api.post('/projects', data, {
                     headers: {
                         Authorization: `Bearer ${accessToken}`
                     }
@@ -33,13 +35,7 @@ export default function ProjectCreateEditForm({ mode, project }) {
                     navigate('/projects');
                 }
             }else{
-                const response = await api.put('/projects/'+project.id, {
-                    name,
-                    description,
-                    // createNew,
-                    // projectAdminId: userId,
-                    // existingProjectMemberUserId: existingUsers?.[0]?.userId
-                }, {
+                const response = await api.put('/projects/'+project.id, data, {
                     headers: {
                         Authorization: `Bearer ${accessToken}`
                     }
@@ -55,24 +51,24 @@ export default function ProjectCreateEditForm({ mode, project }) {
         }
     }
     return (
-        <form className="auth-form project-form" onSubmit={handleSubmit}>
+        <form className="auth-form project-form" onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group">
                 <label>Name</label>
-                <input type="text" placeholder="Enter project name" value={name} onChange={(e) => setProjectName(e.target.value)} />
+                <input type="text" placeholder="Enter project name" {...register('name')} />
+                {errors.name && <p className="error-text">{errors.name.message}</p>}
               </div>
 
               <div className="form-group">
                 <label>Description</label>
-                <textarea name="description" id="description" value={description} onChange={(e) => setProjectDescription(e.target.value)} placeholder="Enter Description"></textarea>
+                <textarea {...register('description')} placeholder="Enter Description"></textarea>
+                {errors.description && <p className="error-text">{errors.description.message}</p>}
               </div>
 
-              <div className={formErrorMessage ? 'error' : 'success'}>
-                <label>{formErrorMessage}</label>
-            </div>
+                <p className="error-text">{formErrorMessage}</p>
 
-              <button type="submit" className="auth-btn">
-                {mode === 'create' ? 'Submit' : 'Update'}
-              </button>
+              <button type="submit" className="auth-btn" disabled={isSubmitting}>
+                {mode === 'create' ? (isSubmitting ? 'Submitting...' : 'Submit') : (isSubmitting ? 'Updating...' : 'Update')}
+                </button>
               <button type="button" className="auth-btn" onClick={() => navigate('/projects')}>
                 Cancel
               </button>
